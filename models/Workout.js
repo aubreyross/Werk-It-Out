2d938af4d42546dff2f82576a8248fa83e51ff0f
aubreyross <aubrey.ross04@gmail.com>
2022-01-18 23:38:30 -0600

    readded workout.jsfile

---
+++ models/Workout.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+      type: Date,
+      default: Date.now
+    },
+
+    exercises: [
+      {
+  
+        type: {
+          type: String,
+          required: "Type of exercise"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Name of workout"
+        },
+        date: {
+          type: Date,
+          default: new Date().setDate
+        },
+        duration: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        },
+        weight: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+      },
+  ],
+
+  totalDuration: {
+    type: Number,
+    },
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
+
